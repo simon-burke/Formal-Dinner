1e2f944f3e9dcfcf6ae0e95687d2b9c4b4851e86
simon-burke <59749432+simon-burke@users.noreply.github.com>
2020-02-05 08:05:41 -0800

    Update Formal_dinner2.py

--- Formal_dinner2.py
+++ Formal_dinner2.py
@@ -1,20 +1,22 @@
 import csv
 import random
 
+##writes tables and kitchen crew in csv file
 def write(filled, waiters, kitchen, file):
     with open(file, 'w') as csvfile:
-        fw = csv.writer(csvfile, delimiter=' ')
+        fw = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+##iterate through filled array and copy table and waiter to csv file
         for i in range(len(filled)-1):
-            fw.writerow(' ')
-            fw.writerow('Table ' + str(i + 1) + ':')
-            fw.writerow(' ')
             for j in range(7):
-                fw.writerow(filled[i][j])
-            fw.writerow(waiters[i] + '(W)')
-        fw.writerow(' ')
-        fw.writerow("Kitchen Crew:")
+                fw.writerow([str(filled[i][j]) + ',' + str(i+1)])
+            fw.writerow([str(waiters[i]) + ',W' + str(i+1)])
+##iterate through kitchen array and copy to csv
         for i in kitchen:
-            fw.writerow(i)
+            fw.writerow([str(i) + ',' + 'kitchen'])
+
+##reads csv file and copy information to arrays
 def read(file):
     students = []
     rows, cols = (36, 7)
@@ -26,13 +28,14 @@ def read(file):
 
     with open(file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ',')
-        line_count = -1
         for row in csv_reader:
             students.append(row[1] + ' ' + row[0])
-            line_count += 1
+##removes jibberish around first name read
         students[0] = "Daanish Ahmad"
 
     return students
+
+##creates first set of tables
 def fill(students):
     rows, cols = (36, 7)
     table = []
@@ -41,14 +44,17 @@ def fill(students):
     waiters = []
     kitchen = []
 
+##iterate through students and randomly assigns one waiter per table
     for i in range(rows):
         rand = random.randint(0,len(students)-1)
         waiters.append(students[rand])
         students.remove(students[rand])
+##iterate through and randomly assign 7 students to kitchen crew
     for i in range(7):
         rand = random.randint(0,len(students)-1)
         kitchen.append(students[rand])
         students.remove(students[rand])
+        ##while there are unassigned students, randomly assign 7 students to each table
     while len(students) > 0:
         for i in range(7):
             if len(students) != 0:
@@ -59,6 +65,8 @@ def fill(students):
         table = []
         tablecount += 1
     return filled,waiters,kitchen
+
+##print output to console
 def out(filled, kitchen, waiters):
     print("Tables: ")
     print(filled)
@@ -69,8 +77,30 @@ def out(filled, kitchen, waiters):
     print("Kitchen Crew:")
     print(kitchen)
 
+##shuffle tables to create second and third seatings
+def shuffle(number ,filled, waiters, kitchen):
+    ##swap kitchen crew with table members depending on the seating number
+    for i in range(7):
+        filled[i][number], kitchen[i] = kitchen[i], filled[i][number]
+        ##swap waiters with table members
+    for i in range(len(filled)):
+        filled[i][0], waiters[i] = waiters[i], filled[i][0]
+##shuffle table members so that first table member moves up one table, second moves up two tables, etc.
+        for j in range(6,0,-1):
+            nextTable = i + j + 1
+            if nextTable > 35:
+                nextTable -= 36
+            filled[nextTable].append(filled[i].pop(j))
+
+
 file = 'student_list.csv'
 students = read(file)
 filled,waiters,kitchen = fill(students)
 write(filled,waiters,kitchen, 'FDTables.csv')
+out(filled, kitchen, waiters)
+shuffle(1, filled, kitchen, waiters)
+write(filled, waiters, kitchen, 'FDTables2.csv')
+out(filled, kitchen, waiters)
+shuffle(2, filled, kitchen, waiters)
+write(filled, waiters, kitchen, 'FDTables3.csv')
 out(filled, kitchen, waiters)
